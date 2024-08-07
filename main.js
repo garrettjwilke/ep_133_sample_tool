2984a3bd0935c94e20870ca05b219667a89b259a
homeless <homeless@homelesss-MacBook-Pro.local>
2024-08-06 18:39:38 -0600

    added icon

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ function createWindow () {
   const mainWindow = new BrowserWindow({
     width: 1200,
     height: 1200,
+    icon: path.join(__dirname + 'icon.ico'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
