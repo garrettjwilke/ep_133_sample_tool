4eb950ef4317fc4850d4ee0d0db4048e57b3c3b9
homeless <homeless@homelesss-MacBook-Pro.local>
2024-08-28 01:49:20 -0600

    custom shizz

--- main.js
+++ main.js
@@ -5,16 +5,17 @@ const path = require('node:path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 1200,
-    height: 1024,
+    width: 1180,
+    height: 900,
+    title: "hmls-133",
     icon: path.join(__dirname + 'icon.ico'),
     webPreferences: {
-      devTools: false,
+      //devTools: false,
       preload: path.join(__dirname, 'preload.js')
     }
   })
   // and load the index.html of the app.
-  mainWindow.setMenuBarVisibility(false)
+  //mainWindow.setMenuBarVisibility(false)
   mainWindow.loadFile('data/index.html')
 
   // Open the DevTools.
@@ -28,18 +29,12 @@ app.whenReady().then(() => {
   createWindow()
 
   app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
   app.quit()
-  //if (process.platform !== 'darwin') app.quit()
 })
 
 // In this file you can include the rest of your app's specific main process
